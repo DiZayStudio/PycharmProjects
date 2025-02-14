7189f4716d83edff627aa6d7859326bdd59cf688
ZaytsevDU <zaytsev@nrtb.ru>
2025-02-14 10:39:05 +0300

    Correction Task_2

--- Task2/main.py
+++ Task2/main.py
@@ -16,11 +16,21 @@ def medium(a, b, c):
             else:
                 return b
 
-#x1, x2, x3 = int(input("Введите целые числа:"))
-x1 = 3
-x2 = 20
-x3 = 1
+numbers_str = input("Введите три целых числа: ")
+numbers = list(map(int, numbers_str.split()))
+x1, x2, x3 = numbers
 
 res = medium(x1, x2, x3)
 print(res)
-#print(medium(x1, x2, x3))
+
+# Task 3_2
+def print_sqr(n):
+    # формируем строку из N символов
+    str = ""
+    for i in n:
+        str += "*"
+    print(str)
+    for k in range(0, n):
+        print("*")
+
+print_sqr(int(input("Введите целое число: ")))
