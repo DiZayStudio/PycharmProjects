51e7fc9fc6756819f6d17f6f7fbb25f2945c6d3a
ZaytsevDU <zaytsev@nrtb.ru>
2025-02-18 08:46:31 +0300

    Done Task_5

--- Task2/play_video.py
+++ Task2/play_video.py
@@ -8,7 +8,7 @@ def annotate(img, x, y, w, h):
     x_end = x_start + int(img.shape[1] * w)
     y_end = y_start + int(img.shape[0] * h)
     # Определение текста и его параметров
-    text = "здесь рамка"
+    text = "Красная рамка"
     font = cv2.FONT_HERSHEY_COMPLEX   # установка шрифта
     font_scale = 0.7                  # размер шрифта
     font_color = (255, 255, 255)      # Белый цвет
@@ -16,8 +16,8 @@ def annotate(img, x, y, w, h):
     line_type = 1                     # тип линии (1-сплошная)
 
     # Позиция текста на изображении
-    text_x = x_start - int(img.shape[1] * w)
-    text_y = y_start - int(img.shape[0] * h) - 10
+    text_x = x_start
+    text_y = y_start - 10
 
     # Добавление текста на изображение
     cv2.putText(img, text, (text_x, text_y), font, font_scale, font_color, line_type)
@@ -40,9 +40,7 @@ if not cap.isOpened():
     print("Ошибка: Не удалось открыть видео.")
     exit()
 # Сохранение в файл
-result = cv2.VideoWriter('filename.avi',  
-                         cv2.VideoWriter_fourcc(*'MJPG'), 
-                         10, size) 
+result = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'XVID'), 25, (1440, 1080))
 
 # Чтение и отображение кадров
 while True:
@@ -52,7 +50,7 @@ while True:
         break  # Выход из цикла, если больше нет кадров
 
     # Рисуем рамку (ДЗ)
-    box = [0.45, 0.45, 0.1, 0.1]  # относительные координаты рамки
+    box = [0.4, 0.4, 0.1, 0.1]  # относительные координаты рамки
     image = annotate(frame, *box)
     # Отображение текущего кадра с помощью OpenCV
     cv2.imshow('Video Playback', image)
